31801cd3213517b16cff1ac6dec6a2278c05b2c3
djarpin <arpin.david@gmail.com>
2014-03-29 23:00:25 -0500

    updated plr comments and output

--- examples/regression_by_income_r.sql
+++ examples/regression_by_income_r.sql
@@ -1,8 +1,8 @@
 -- Run a linear regression in R
-\o plr_temp.log
+\o regression_by_income_r.log
 \timing
 
--- Create a PL/R function
+-- Create a PL/R regression function
 drop function regression_r(float8[]) cascade;
 create or replace function regression_r(float8[]) returns float8 as
 $$
@@ -14,13 +14,13 @@ $$
 $$
 language plr;
 
--- create function for aggregate
+-- Create a function to pack x and y data into a single array
 create or replace function regression_r_accum(float8[], float8, float8) returns float8[] as
 $$
   select case when $1 is null then array[$2, $3] else $1 || $2 || $3 end
 $$ language sql;
 
--- create aggregate
+-- Create aggregate which allows for group by use
 drop aggregate if exists regression_r_agg(float8, float8);
 create aggregate regression_r_agg(float8, float8) (
   sfunc = regression_r_accum,
@@ -28,7 +28,7 @@ create aggregate regression_r_agg(float8, float8) (
   finalfunc = regression_r
 );
 
--- Call PL/R function by income
+-- Call PL/R regression function by income
 select yearly_income,
        regression_r_agg(sales, total_children) as beta
 from regression
